600a36276c7f681d31977f49a28eef1ca5570dbc
Yong Yao Wen <89002614+Wen-MY@users.noreply.github.com>
2024-01-20 00:08:09 +0800

    Update firebase-config.js

--- server/firebase-config.js
+++ server/firebase-config.js
@@ -2,12 +2,12 @@ import firebase from 'firebase/app';
 import 'firebase/storage';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDEQ6aehqAg32heF6PnYav6_F4oozz5vSg",
-    authDomain: "virtual-hair-studio.firebaseapp.com",
-    projectId: "virtual-hair-studio",
-    storageBucket: "virtual-hair-studio.appspot.com",
-    messagingSenderId: "260751415958",
-    appId: "1:260751415958:web:1145324cc43d502e8440b7"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
   };
 
 firebase.initializeApp(firebaseConfig);
